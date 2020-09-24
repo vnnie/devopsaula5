d6b910c70c861b32df58c70326db38168bfd1d7f
vnnie <vinicius.barbosa@aluno.faculdadeimpacta.com.br>
2020-09-24 15:38:50 -0300

    AC

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -3,7 +3,7 @@ def inicializar():
 	for i in range(3):
 		linha = [ ]
 		for j in range(3):
-                    linha.ap pend(".")
+                    linha.append(".")
 		tab.append(linha)
 	return tab
 
